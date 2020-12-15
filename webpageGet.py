0615c4ab28e4bf7191ec32d3f0a69e21d5ef5725
1amn0body <firewolf6342@gmail.com>
2020-12-15 22:24:03 +0100

    Initial commit

---
+++ webpageGet.py
@@ -0,0 +1,61 @@
+import requests
+from bs4 import BeautifulSoup as bs
+
+def findInfos(isbn):
+    infos = []
+
+    url = "https://www.thalia.de/suche?sq="
+    html_doc = requests.get(url + isbn).text
+
+    soup = bs(html_doc, 'html.parser')
+
+    #find section
+    section = soup.find('section', {'class': ["artikel-details"]})
+    #print(section.prettify())
+
+    if section is not None:
+        #find title
+        try:
+            title = section.find('h3').decode_contents()
+            infos.append(title)
+        except Exception as e:
+            infos.append(None)
+
+        #find series
+        try:
+            series = section.find('a', {'class': ["serie"]}).decode_contents()
+            infos.append(series)
+        except Exception as e:
+            infos.append(None)
+
+        #find author(s)
+        try:
+            author_s = section.find('ul', {'class': ["autoren-liste"]}).find_all('a')
+
+            for i in range(len(author_s)):
+                author_s[i] = author_s[i].decode_contents()
+
+            infos.append(author_s)
+        except Exception as e:
+            infos.append(None)
+
+
+        return infos
+    return None
+
+
+#isbn = ["9783442268160", "9780553499148", "9781420958713", "9783423252812", "9783426281550", "3426281554", "3841907350"]
+
+#for i in isbn:
+#    print(findInfos(i))
+
+
+while True:
+    print()
+    ipt = input("Eine ISBN oder 'X' angeben: ").lower()
+
+    if ipt == "x":
+        print("Beende...")
+        break
+    else:
+        print(findInfos(ipt))
